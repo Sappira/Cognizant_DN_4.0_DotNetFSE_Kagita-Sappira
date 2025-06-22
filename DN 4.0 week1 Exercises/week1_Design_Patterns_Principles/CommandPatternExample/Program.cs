19bd0fad089a8fc9898baac3f82ee4f3f0346315
Sappira <lenikagita@gmail.com>
2025-06-22 22:26:17 +0530

    Add files via upload

---
+++ DN 4.0 week1 Exercises/week1_Design_Patterns_Principles/CommandPatternExample/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+// Command Interface
+public interface ICommand
+{
+    void Execute();
+}
+
+// Receiver
+public class Light
+{
+    public void TurnOn()
+    {
+        Console.WriteLine("Light is ON");
+    }
+
+    public void TurnOff()
+    {
+        Console.WriteLine("Light is OFF");
+    }
+}
+
+// Concrete Commands
+public class LightOnCommand : ICommand
+{
+    private readonly Light _light;
+
+    public LightOnCommand(Light light)
+    {
+        _light = light;
+    }
+
+    public void Execute()
+    {
+        _light.TurnOn();
+    }
+}
+
+public class LightOffCommand : ICommand
+{
+    private readonly Light _light;
+
+    public LightOffCommand(Light light)
+    {
+        _light = light;
+    }
+
+    public void Execute()
+    {
+        _light.TurnOff();
+    }
+}
+
+// Invoker
+public class RemoteControl
+{
+    private ICommand _command;
+
+    public void SetCommand(ICommand command)
+    {
+        _command = command;
+    }
+
+    public void PressButton()
+    {
+        _command.Execute();
+    }
+}
+
+// Main
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Light livingRoomLight = new Light();
+        
+        ICommand lightOn = new LightOnCommand(livingRoomLight);
+        ICommand lightOff = new LightOffCommand(livingRoomLight);
+
+        RemoteControl remote = new RemoteControl();
+
+        remote.SetCommand(lightOn);
+        remote.PressButton();
+
+        remote.SetCommand(lightOff);
+        remote.PressButton();
+    }
+}
