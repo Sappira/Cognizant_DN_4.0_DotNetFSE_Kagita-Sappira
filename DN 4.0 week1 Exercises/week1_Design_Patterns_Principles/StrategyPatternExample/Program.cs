8e39042cd8641e8fcb15f46416aa5b7155bc4bea
Sappira <lenikagita@gmail.com>
2025-06-22 22:14:18 +0530

    Add files via upload

---
+++ DN 4.0 week1 Exercises/week1_Design_Patterns_Principles/StrategyPatternExample/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public interface IPaymentStrategy
+{
+    void Pay(double amount);
+}
+
+public class CreditCardPayment : IPaymentStrategy
+{
+    public void Pay(double amount) => Console.WriteLine($"Paid {amount:C} with Credit Card");
+}
+
+public class PayPalPayment : IPaymentStrategy
+{
+    public void Pay(double amount) => Console.WriteLine($"Paid {amount:C} with PayPal");
+}
+
+public class PaymentContext
+{
+    private IPaymentStrategy _strategy;
+
+    public void SetStrategy(IPaymentStrategy strategy) => _strategy = strategy;
+
+    public void Pay(double amount)
+    {
+        if (_strategy == null)
+            Console.WriteLine("No strategy set.");
+        else
+            _strategy.Pay(amount);
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        var context = new PaymentContext();
+
+        context.SetStrategy(new CreditCardPayment());
+        context.Pay(500);
+
+        context.SetStrategy(new PayPalPayment());
+        context.Pay(750);
+    }
+}
